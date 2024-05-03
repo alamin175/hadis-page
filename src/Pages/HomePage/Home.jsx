f96ce17763ab14bf4ec6262c1e92131258d7b2aa
Md. Al-Amin <mdalaminh052@gmail.com>
2024-05-03 23:11:54 +0600

    some issue fixed

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
 	return (
-		<div className="flex flex-col m-6  mx-auto">
+		<div className="flex flex-col m-6 w-screen h-screen items-center mx-auto">
 			<h1 className="text-emerald-600 font-bold m-6 text-3xl">
 				Please Go to Hadis
 			</h1>
-			<button className="btn text-white font-bold text-xl bg-emerald-600 hover:bg-emerald-700">
+			<button className="btn w-36 text-white font-bold text-xl bg-emerald-600 hover:bg-emerald-700">
 				<Link to="/bukhari">Hadis Page</Link>
 			</button>
 		</div>
